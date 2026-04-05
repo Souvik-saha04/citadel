5a71cf3b34f1bf611cb870c8c95018bbd8630f8c
Souvik Saha <souviksh2004@gmail.com>
2026-04-05 12:11:54 +0530

    frontend url done

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,11 +74,10 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
-# CORS_ALLOWED_ORIGINS=[
-#     "http://localhost:5173",
-#     "http://127.0.0.1:5173",
-# ]
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS=[
+    "https://greengrocers-one.vercel.app/",
+    ]
+
 
 """ CSRF_TRUSTED_ORIGINS = ["http://localhost:5173",]
 
